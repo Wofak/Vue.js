18b9a6100a8c095aba2385aeba5329791557dceb
Wofak <wodstra@protonmail.com>
2021-07-05 17:26:11 +0900

    Create Class.vue

---
+++ test04/src/views/Class.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container" v-bind:class="{'active':isActive, 'text-red':hasError}">Class Binding</div>
+</template>
+<script>
+export default {
+    name: "",
+    components: {},
+    data() {
+        return {
+            isActive: true,
+            hasError: true,
+        };
+    },
+    setup() {},
+    created() {},
+    mounted() {},
+    unmounted() {},
+    methods: {}
+}
+</script>
+<style scoped>
+/* scoped를 적용하면 해당 components 내에서만 먹힘 */
+.container {
+    width: 100%;
+    height: 200px;
+}
+.active { 
+    background-color: yellow;
+    font-weight: bold;
+}
+.text-red{
+    color: red;
+}
+
+</style>
